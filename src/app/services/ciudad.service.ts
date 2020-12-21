f0367cf3549fa6347b4c80328689c17e89560908
Jesus <jesuscardenasb85@gmail.com>
2020-12-21 08:22:59 -0300

    SC

--- src/app/services/ciudad.service.ts
+++ src/app/services/ciudad.service.ts
@@ -17,26 +17,4 @@ export class CiudadService {
     return this.httpService.get<any>(this.URL);
   }
 
-  getDocument(id: string) {
-    this.socket.emit('getDoc', id);
-  }
-
-  newDocument() {
-    this.socket.emit('addDoc', { id: this.docId(), doc: '' });
-  }
-
-  editDocument(document: Document) {
-    this.socket.emit('editDoc', document);
-  }
-
-  private docId() {
-    let text = '';
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-    for (let i = 0; i < 5; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-
-    return text;
-  }
 }
